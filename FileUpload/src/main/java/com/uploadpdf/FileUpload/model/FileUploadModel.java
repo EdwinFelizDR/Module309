7c9c71b2956d0a26618ee337b91278e48089ca03
EdwinFelizDR <efeliz12lg@gmail.com>
2024-08-30 10:18:13 -0400

    Guided LAB - 309.9.2 - AC17 - Spring Boot - Upload PDF File

---
+++ FileUpload/src/main/java/com/uploadpdf/FileUpload/model/FileUploadModel.java
@@ -0,0 +1,12 @@
+package com.uploadpdf.FileUpload.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Getter
+@Setter
+@NoArgsConstructor
+public class FileUploadModel {
+    private String uploadedFile;
+}
+
