4143542d94f8e44a8b2cf868a0d14cd86708e29c
EdwinFelizDR <efeliz12lg@gmail.com>
2024-09-03 09:05:23 -0400

    GLAB 310.1.2 - SP19 - Spring Boot SOAP Web service

---
+++ SpringSOAPdemo/src/main/java/perscholas/SpringSOAPdemo/SpringSoaPdemoApplication.java
@@ -0,0 +1,14 @@
+package perscholas.SpringSOAPdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSoaPdemoApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringSoaPdemoApplication.class, args);
+	}
+
+}
---
+++ SpringSOAPdemo/src/test/java/perscholas/SpringSOAPdemo/SpringSoaPdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package perscholas.SpringSOAPdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSoaPdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
